fe3115529bb9ec2646399f3ce77830333deda878
Sven <warlord12@freenet.de>
2020-11-19 09:11:05 +0100

    temp fix

--- FC3.lua
+++ FC3.lua
@@ -27,16 +27,19 @@ local _adiYaw = 0.0
 
 local function LoGetSelfPlane()
 	local self = LoGetSelfData()
+	if self == nil then self = "XXX" end
 	return self.Name
 end
 
 local function LoGetFuelAll()
 	local eng = LoGetEngineInfo()
+	if eng == nil then return end
 	return eng.fuel_internal + eng.fuel_external
 end
 
 local function LoGetGLoad()
 	local au = LoGetAccelerationUnits()
+	if au == nil then return end
 	return au.y
 end
 
@@ -52,19 +55,19 @@ local function BarFuel(fuel, plane)
 	elseif plane == "Su-27" or plane == "Su-33" or plane == "J-11A" then maxFuel = 9400
 	end
 
-	local bar = math.ceil((fuel / maxFuel) * 16)
-	if bar > 16 then bar = 16 end
-
-	return bar
+	local barf = math.ceil((fuel / maxFuel) * 16)
+	if barf > 16 then barf = 16 end
+    if barf == nil then return end
+	return barf
 end
 
 local function BarGLoad(gload)
-	local bar = math.floor((gload / 11) * 16)
+	local barg = math.floor((gload / 11) * 16)
 
-	if bar < 0 then bar = 0
-	elseif bar > 16 then bar = 16 end
-
-	return bar
+	if barg < 0 then barg = 0
+	elseif barg > 16 then barg = 16 end
+    if barg == nil then return end
+	return barg
 end
 
 local function BarVVI(vvi, plane)
@@ -77,7 +80,7 @@ local function BarVVI(vvi, plane)
 	elseif bar > 8 then bar = 8
 	elseif bar > 0 then bar = math.ceil(bar)
 	end
-
+    if bar == nil then return end
 	return bar + 8
 end
 
@@ -97,16 +100,16 @@ moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
 	local self = LoGetSelfData()
 	local plane = LoGetSelfPlane()
 
-	local altS = LoGetAltitudeAboveSeaLevel()
-	local altG = LoGetAltitudeAboveGroundLevel()
-	local alt = altS
-	local vvi = LoGetVerticalVelocity()
-	local ias = LoGetIndicatedAirSpeed()
-	local tas = LoGetTrueAirSpeed()
-	local mach = LoGetMachNumber()
-	local fuel = LoGetFuelAll()
-	local aoa = LoGetAngleOfAttack()
-	local gload = LoGetGLoad()
+	local altS = LoGetAltitudeAboveSeaLevel() or 0
+	local altG = LoGetAltitudeAboveGroundLevel() or 0
+	local alt = altS or 0
+	local vvi = LoGetVerticalVelocity() or 0
+	local ias = LoGetIndicatedAirSpeed() or 0
+	local tas = LoGetTrueAirSpeed() or 0
+	local mach = LoGetMachNumber() or 0
+	local fuel = LoGetFuelAll() or 0
+	local aoa = LoGetAngleOfAttack() or 0
+	local gload = LoGetGLoad() or 0
 	_glidedev = LoGetGlideDeviation()
 	_sidedev = LoGetSideDeviation()
 	_slipball = LoGetSlipBallPosition()
@@ -114,6 +117,14 @@ moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
 	_barFuel = BarFuel(fuel, plane)
 	_barGLoad = BarGLoad(gload)
 	_barVVI = BarVVI(vvi, plane)
+	
+	-- _RPMLeft = LoGetEngineInfo().RPM.left 
+	-- _RPMRight = LoGetEngineInfo().RPM.right 
+	-- _TEMPLeft = LoGetEngineInfo().Temperature.left 
+	-- _TEMPRight = LoGetEngineInfo().Temperature.right 
+	-- _GearStatus = LoGetMechInfo().gear.value 	
+	-- _chaff = LoGetSnares().chaff 
+	-- _flare = LoGetSnares().flare 
 
 	--[[ US PLANES ]]--
 	if plane == "A-10A" or plane == "F-15C" or plane == "MiG-29G" then
@@ -181,7 +192,8 @@ moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
 
 	if fuel > 100 then _fuel = string.format("%4d", fuel)
 	else _fuel = string.format("%4.1f", fuel) end
-
+     if _fuel == nil then return end
+	 
 	-- G LOAD
 	if plane == "A-10A" then
 		if gload < -5 then gload = -5
@@ -205,43 +217,33 @@ moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
 	else _verticalVelocity = string.format("%4.1f", vvi) end
 
 	_adiPitch, _adiBank, _adiYaw = LoGetADIPitchBankYaw()
-	
-	_RPMLeft = LoGetEngineInfo().RPM.left
-	_RPMRight = LoGetEngineInfo().RPM.right
-	_TEMPLeft = LoGetEngineInfo().Temperature.left
-	_TEMPRight = LoGetEngineInfo().Temperature.right
-	
-	_GearStatus = LoGetMechInfo().gear.value
-	
-	_chaff = LoGetSnares().chaff
-	_flare = LoGetSnares().flare
 end
 
 --Altitude
-defineString("FC3_ALTITUDE", function() return _altitude .. string.char(0) end, 6, "Altitude", "Altitude")
-defineString("FC3_ALTITUDE_GROUND", function() return _altitudeG .. string.char(0) end, 6, "Altitude", "Altitude above Ground")
-defineString("FC3_ALTITUDE_SEA", function() return _altitudeS .. string.char(0) end, 6, "Altitude", "Altitude above Sea Level")
-defineString("FC3_ANGLE_OF_ATTACK", function() return _AoA .. string.char(0) end, 4, "String", "Angle of Attack")
-defineString("FC3_FUEL_ALL", function() return _fuel .. string.char(0) end, 5, "String", "Fuel Remaining")
-defineString("FC3_G_LOAD", function() return _gLoad .. string.char(0) end, 4, "String", "G Load")
-defineString("FC3_INDICATED_AIRSPEED", function() return _indicatedAirspeed .. string.char(0) end, 4, "Speed", "Indicated Airspeed")
-defineString("FC3_MACH_NUMBER", function() return _machNumber .. string.char(0) end, 4, "Speed", "Mach Number")
-defineString("FC3_TRUE_AIRSPEED", function() return _trueAirspeed .. string.char(0) end, 4, "Speed", "True Airspeed")
-defineString("FC3_VERTICAL_VELOCITY", function() return _verticalVelocity .. string.char(0) end, 4, "Speed", "Vertical Velocity")
+defineString("FC3_ALTITUDE", function() return _altitude .. string.char(0) or "000000" end, 6, "Altitude", "Altitude")
+defineString("FC3_ALTITUDE_GROUND", function() return _altitudeG .. string.char(0) or "000000" end, 6, "Altitude", "Altitude above Ground")
+defineString("FC3_ALTITUDE_SEA", function() return _altitudeS .. string.char(0) or "000000"  end, 6, "Altitude", "Altitude above Sea Level")
+defineString("FC3_ANGLE_OF_ATTACK", function() return _AoA .. string.char(0) or "0000" end, 4, "String", "Angle of Attack")
+defineString("FC3_FUEL_ALL", function() return _fuel .. string.char(0) or "00000" end, 5, "String", "Fuel Remaining")
+defineString("FC3_G_LOAD", function() return _gLoad .. string.char(0) or "0000" end, 4, "String", "G Load")
+defineString("FC3_INDICATED_AIRSPEED", function() return _indicatedAirspeed .. string.char(0) or "0000" end, 4, "Speed", "Indicated Airspeed")
+defineString("FC3_MACH_NUMBER", function() return _machNumber .. string.char(0) or "0000" end, 4, "Speed", "Mach Number")
+defineString("FC3_TRUE_AIRSPEED", function() return _trueAirspeed .. string.char(0) or "0000" end, 4, "Speed", "True Airspeed")
+defineString("FC3_VERTICAL_VELOCITY", function() return _verticalVelocity .. string.char(0) or "0000" end, 4, "Speed", "Vertical Velocity")
 defineIntegerFromGetter("FC3_RADAR_ALTITUDE", function() return _radarAltitude end, 1, "Altitude", "Radar Altitude")
 
 --Engine
-defineString("FC3_RPM_L", function() return _RPMLeft .. string.char(0) end, 3, "Engine", "RPM Left Engine")
-defineString("FC3_RPM_R", function() return _RPMRight .. string.char(0) end, 3, "Engine", "RPM Left Engine")
-defineString("FC3_TEMP_L", function() return _TEMPLeft .. string.char(0) end, 3, "Engine", "Temperature Left Engine")
-defineString("FC3_TEMP_R", function() return _TEMPRight .. string.char(0) end, 3, "Engine", "Temperature Left Engine")
+-- defineString("FC3_RPM_L", function() return _RPMLeft .. string.char(0) or "00000" end, 5, "Engine", "RPM Left Engine")
+-- defineString("FC3_RPM_R", function() return _RPMRight .. string.char(0) or "00000" end, 5, "Engine", "RPM Left Engine")
+-- defineString("FC3_TEMP_L", function() return _TEMPLeft .. string.char(0) or "000" end, 3, "Engine", "Temperature Left Engine")
+-- defineString("FC3_TEMP_R", function() return _TEMPRight .. string.char(0) or "000" end, 3, "Engine", "Temperature Left Engine")
 
 --Mechanical
-defineString("FC3_GEAR", function() return _GearStatus .. string.char(0) end, 1, "Mechanical", "Gear Status")
+-- defineString("FC3_GEAR", function() return _GearStatus .. string.char(0) or "0" end, 1, "Mechanical", "Gear Status")
 
 --Countermeasures
-defineString("FC3_CHAFF", function() return _chaff .. string.char(0) end, 3, "Countermeasures", "Chaff Counter")
-defineString("FC3_FLARE", function() return _flare .. string.char(0) end, 3, "Countermeasures", "Flare Counter")
+-- defineString("FC3_CHAFF", function() return _chaff .. string.char(0) or "000" end, 3, "Countermeasures", "Chaff Counter")
+-- defineString("FC3_FLARE", function() return _flare .. string.char(0) or "000" end, 3, "Countermeasures", "Flare Counter")
 
 --Bar
 defineIntegerFromGetter("FC3_FUEL_BAR", function() return _barFuel end, 16, "Bar", "Fuel Bar")
@@ -249,12 +251,12 @@ defineIntegerFromGetter("FC3_G_LOAD_BAR", function() return _barGLoad end, 16, "
 defineIntegerFromGetter("FC3_VVI_BAR", function() return _barVVI end, 16, "Bar", "Vertical Velocity Bar")
 
 --Float
-define8BitFloatFromGetter("FC3_ADI_BANK", function() return _adiBank end, {-1, 1}, "Float", "ADI Bank")
-define8BitFloatFromGetter("FC3_ADI_PITCH", function() return _adiPitch end, {-1, 1}, "Float", "ADI Pitch")
-define8BitFloatFromGetter("FC3_ADI_YAW", function() return _adiYaw end, {-1, 1}, "Float", "ADI Yaw")
-define8BitFloatFromGetter("FC3_GLIDE_DEVIATION", function() return _glidedev end, {-1, 1}, "Float", "Glide Deviation")
-define8BitFloatFromGetter("FC3_SIDE_DEVIATION", function() return _sidedev end, {-1, 1}, "Float", "Side Deviation")
-define8BitFloatFromGetter("FC3_SLIP_BALL_POSITION", function() return _slipball end, {-1, 1}, "Float", "Slip Ball Position")
+define8BitFloatFromGetter("FC3_ADI_BANK", function() return _adiBank or 0 end, {-1, 1}, "Float", "ADI Bank")
+define8BitFloatFromGetter("FC3_ADI_PITCH", function() return _adiPitch or 0 end, {-1, 1}, "Float", "ADI Pitch")
+define8BitFloatFromGetter("FC3_ADI_YAW", function() return _adiYaw or 0 end, {-1, 1}, "Float", "ADI Yaw")
+define8BitFloatFromGetter("FC3_GLIDE_DEVIATION", function() return _glidedev or 0 end, {-1, 1}, "Float", "Glide Deviation")
+define8BitFloatFromGetter("FC3_SIDE_DEVIATION", function() return _sidedev or 0 end, {-1, 1}, "Float", "Side Deviation")
+define8BitFloatFromGetter("FC3_SLIP_BALL_POSITION", function() return _slipball or 0 end, {-1, 1}, "Float", "Slip Ball Position")
 
 --Externals
 defineIntegerFromGetter("EXT_SPEED_BRAKE_RIGHT", function()
@@ -273,5 +275,15 @@ end, 1, "External Aircraft Model", "Right Position Light (green)")
 defineIntegerFromGetter("EXT_STROBE", function()
 	if LoGetAircraftDrawArgumentValue(192) > 0 then return 1 else return 0 end
 end, 1, "External Aircraft Model", "Strobe Light")
+
+defineIntegerFromGetter("EXT_WOW_NOSE", function()
+	if LoGetAircraftDrawArgumentValue(1) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Nose Gear")
+defineIntegerFromGetter("EXT_WOW_RIGHT", function()
+	if LoGetAircraftDrawArgumentValue(4) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Right Gear")
+defineIntegerFromGetter("EXT_WOW_LEFT", function()
+	if LoGetAircraftDrawArgumentValue(6) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Left Gear")
 
 BIOS.protocol.endModule()
